168d056bedbc0fcbf76f5d42554c83539d2e8159
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2023-08-12 01:09:25 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb+srv://alokkumarrai08022003:UeOcunueSQyMYz4t@cluster0.l1ej37e.mongodb.net/?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
